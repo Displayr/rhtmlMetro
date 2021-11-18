4d2b8448c11b8c55e7c221dcbe4951ba1a487693
Justin Yap <JustinCCYap@users.noreply.github.com>
2021-11-18 11:38:29 +1100

    Remove semicolons

--- theSrc/scripts/rhtmlMetro.factory.jest.test.js
+++ theSrc/scripts/rhtmlMetro.factory.jest.test.js
@@ -2,6 +2,6 @@ const rhtmlMetro_factory = require('./rhtmlMetro.factory')
 
 test('VIS-1000: no error if resize called before renderValue', () => {
   const el = document.createElement('div')
-  const obj = rhtmlMetro_factory(el);
-  obj.resize(); // renderValue not called
+  const obj = rhtmlMetro_factory(el)
+  obj.resize() // renderValue not called
 })
