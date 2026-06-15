1cb5e6e4b5818d77a417191864ca41803538248e
chschan <carmen.chan@displayr.com>
2026-06-15 14:07:56 +1000

    Customize sanitize config

--- theSrc/scripts/Ellipse.js
+++ theSrc/scripts/Ellipse.js
@@ -1,6 +1,7 @@
 const _ = require('lodash')
 const d3 = require('d3')
 const DOMPurify = require('dompurify')
+const SANITIZE_CONFIG = require('./sanitizeConfig')
 
 class Ellipse {
   static initClass () {
@@ -98,7 +99,7 @@ class Ellipse {
         .style('text-decoration', (font_strikethrough) ? 'line-through' : (font_underline) ? 'underline' : '')
 
       if (as_html) {
-        box.html(DOMPurify.sanitize(text))
+        box.html(DOMPurify.sanitize(text, SANITIZE_CONFIG))
       } else {
         box.text(text)
       }
--- theSrc/scripts/Rectangle.js
+++ theSrc/scripts/Rectangle.js
@@ -1,6 +1,7 @@
 const _ = require('lodash')
 const d3 = require('d3')
 const DOMPurify = require('dompurify')
+const SANITIZE_CONFIG = require('./sanitizeConfig')
 
 class Rectangle {
   static initClass () {
@@ -105,7 +106,7 @@ class Rectangle {
         })
 
       if (as_html) {
-        box1.html(DOMPurify.sanitize(text))
+        box1.html(DOMPurify.sanitize(text, SANITIZE_CONFIG))
       } else {
         box1.text(text)
       }
