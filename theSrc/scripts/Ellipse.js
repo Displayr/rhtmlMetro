584cd13e771149a016e174a68bd72176107d7693
Justin Yap <justin.yap@displayr.com>
2026-06-16 10:19:14 +1000

    Revert "RS-22478: fix stored XSS in metro HTML labels (#51)"
    This reverts commit f95425c703574a0aea77af341d0904b16c8aba84.

--- theSrc/scripts/Ellipse.js
+++ theSrc/scripts/Ellipse.js
@@ -1,7 +1,5 @@
 const _ = require('lodash')
 const d3 = require('d3')
-const DOMPurify = require('dompurify')
-const SANITIZE_CONFIG = require('./sanitizeConfig')
 
 class Ellipse {
   static initClass () {
@@ -99,7 +97,7 @@ class Ellipse {
         .style('text-decoration', (font_strikethrough) ? 'line-through' : (font_underline) ? 'underline' : '')
 
       if (as_html) {
-        box.html(DOMPurify.sanitize(text, SANITIZE_CONFIG))
+        box.html(text)
       } else {
         box.text(text)
       }
